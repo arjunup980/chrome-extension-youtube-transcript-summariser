a2da2b611cd6ac48f29c88b1ef0b436684c49958
Arjun Upadhyay <arjunupadhyay@Arjuns-MacBook-Air.local>
2023-04-13 21:20:32 +0530

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the Django application
-gunicorn src.transcript.wsgi:application --bind=0.0.0.0:8000 --workers=4 --settings transcript.settings.production
+gunicorn src.transcript.wsgi:application --bind=0.0.0.0:80 --workers=4 --settings transcript.settings.production
